335249c1461a73bb2bdd5c323d32c03c496c5ebf
deanQj <dean0_o@icloud.com>
2017-01-03 15:26:15 +0800

    update .gitigonore

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var UsersDAO = require('../models/users');
  * 后台首页
  */
 router.get('/', function(req, res, next) {
-  	res.render('index', { title: '首页', baseUrl: req.baseUrl});
+  res.render('index', { title: '首页', baseUrl: req.baseUrl});
 });
 
 module.exports = router;
